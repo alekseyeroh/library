e021ccbd7a1e3cbc65787bd26120724c986c8dfb
Aleksey Eroh <alekseyeroh@gmail.com>
2025-04-13 20:09:15 +0300

    feat: update html css js files

--- main/script.js
+++ main/script.js
@@ -1,10 +1,53 @@
+//Slider
 const dots = document.querySelector('.dots');
-
-dots.addEventListener('click', function(e) {
+const dot = document.querySelectorAll('.dot')
+dots.addEventListener('click', function (e) {
   // if (!e.target.classList.contains('dot'))         // если клик не по ссылке, просто завершим выполнение 
   //   return; 
   // e.preventDefault();                                // отменим стандартную реакцию ссылки на клик
   for (let link of this.querySelectorAll('.dot'))  // удалим класс active у всех наших ссылок... 
-    link.classList.remove('active'); 
+    link.classList.remove('active');
   e.target.classList.add('active');                  // ...и добавим его той, по которой выполнен клик
-}); 
+});
+
+const slides = document.querySelector('.carousel_items');
+const slide = document.querySelectorAll('.carousel_item');
+
+let currentIndex = 0;
+const totalSlides = slide.length;
+
+function updateSliderPosition() {
+  slides.style.transform = `translateX(-${currentIndex * 34}%)`;
+}
+
+console.log(Number(dot[1].value));
+
+dots.addEventListener('click', () => {
+  for (let index = 0; index < dots.length; index++) {
+    if (Number(dot[index].value) == 1) {
+      currentIndex = 1;
+      console.log('ass');
+    } else if (Number(dot[index].value) == 2) {
+      currentIndex = 2;
+      console.log('ass beach');
+    } else currentIndex ===0;
+  }
+  updateSliderPosition();
+});
+
+//FavoriteItems
+let favoritesItems = document.querySelectorAll('.favorites_items');
+let radio = document.querySelectorAll('.contactChoice');
+
+function deleteClass() {
+  for (let b of favoritesItems) {
+    b.classList.remove('open');
+  }
+}
+
+radio.forEach((element, index) => {
+  element.addEventListener('change', () => {
+    deleteClass();
+    favoritesItems[index].classList.add('open')
+  })
+});
