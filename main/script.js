dd91798e1c1b5966eb61750d8af4b43e42c71d64
Aleksey Eroh <alekseyeroh@gmail.com>
2025-04-13 11:59:21 +0300

    feat: update html css js files and upload img

--- main/script.js
+++ main/script.js
@@ -1,9 +1,9 @@
 const dots = document.querySelector('.dots');
 
 dots.addEventListener('click', function(e) {
-  if (!e.target.classList.contains('dot'))         // если клик не по ссылке, просто завершим выполнение 
-    return; 
-  e.preventDefault();                                // отменим стандартную реакцию ссылки на клик
+  // if (!e.target.classList.contains('dot'))         // если клик не по ссылке, просто завершим выполнение 
+  //   return; 
+  // e.preventDefault();                                // отменим стандартную реакцию ссылки на клик
   for (let link of this.querySelectorAll('.dot'))  // удалим класс active у всех наших ссылок... 
     link.classList.remove('active'); 
   e.target.classList.add('active');                  // ...и добавим его той, по которой выполнен клик
