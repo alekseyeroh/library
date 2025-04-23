267dd5b62f222c46390cf92019a1eab720109eea
Aleksey Eroh <alekseyeroh@gmail.com>
2025-04-23 22:24:42 +0300

    feat: update html css js files

--- main/script.js
+++ main/script.js
@@ -58,7 +58,7 @@ radio.forEach((element, index) => {
 let position = 0;
 const slidesToShow = 3;
 const slidesToScroll = 1;
-const container = document.querySelector('.carusel_container');
+const container = document.querySelector('.carousel_container');
 const track = document.querySelector('.carousel_items');
 const items = document.querySelectorAll('.carousel_item');
 const btnPrev = document.querySelector('.dotOne');
@@ -67,12 +67,12 @@ const itemsCount = items.length;
 const itemWidth = container.clientWidth / slidesToShow;
 const movePosition = slidesToScroll * itemWidth;
 
-items.forEach((item) =>{
+items.forEach((item) => {
   item.style.minWidth =`${itemWidth}px`;
 })
 
 btnNext.addEventListener ('click', () => {
-  const itemsLeft = Math.abs(position) / itemWidth;
+  const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
   position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
   setPosition();
   checkBtns();
@@ -87,7 +87,7 @@ btnPrev.addEventListener ('click', () => {
 
 const setPosition = () => {
   track.style.transform = `translateX(${position}px)`;
-}
+};
 
 const checkBtns = () => {
   btnPrev.disabled = position === 0;
